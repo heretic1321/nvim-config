2c80062add470142964015dd4a7f68cca9aaa4b3
heretic1321 <dhruvsmanral13@gmail.com>
2025-08-22 17:08:59 +0000

    feat(keymaps): create keymaps for multipane methods

--- init.lua
+++ init.lua
@@ -21,6 +21,36 @@ vim.keymap.set("n", "<C-u>","<C-u>zz", { noremap = true})
 vim.o.number = true
 vim.o.relativenumber = true
 
+vim.keymap.set("n", "<leader>|", "<cmd>vsplit<cr>")
+vim.keymap.set("n", "<leader>-", "<cmd>split<cr>")
+
+-- move focus
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+
+-- H/L = width  (Left = narrower, Right = wider)
+-- J/K = height (Down = shorter, Up = taller)
+vim.keymap.set("n", "<leader>H", "<cmd>vertical resize -8<cr>", { desc = "Narrower" })
+vim.keymap.set("n", "<leader>L", "<cmd>vertical resize +8<cr>", { desc = "Wider" })
+vim.keymap.set("n", "<leader>J", "<cmd>resize -4<cr>",           { desc = "Shorter" })
+vim.keymap.set("n", "<leader>K", "<cmd>resize +4<cr>",           { desc = "Taller" })
+
+-- Equalize all splits
+vim.keymap.set("n", "<leader>=", "<C-w>=", { desc = "Equalize splits" })
+
+-- --- Close / keep-only ---
+-- Close the current split (keeps the buffer open elsewhere)
+vim.keymap.set("n", "<leader>q", "<cmd>close<cr>", { desc = "Close split" })
+
+-- Keep only this split (close others)
+vim.keymap.set("n", "<leader>o", "<cmd>only<cr>", { desc = "Only this split" })
+
+-- Optional: quit Neovim quickly
+vim.keymap.set("n", "<leader>Q", "<cmd>q<cr>", { desc = "Quit" })
+vim.opt.clipboard:append("unnamedplus")
+
 
 require("vim-options")
 require("lazy").setup("plugins")
